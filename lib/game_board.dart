1643b85f6f1832b1abc624793c052c8abd1b38d4
Each9084 <each9084@gmail.com>
2024-11-03 19:28:23 +0000

    add the new board positon

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -49,15 +49,86 @@ class _GameBoardState extends State<GameBoard> {
     }
 
     //place rooks
-    //place knights
-    //place bishops
-    //place queens
-    //place kings
+    newBoard[0][0] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: false,
+        imagePath: 'lib/images/rook.png');
 
+    newBoard[0][7] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: false,
+        imagePath: 'lib/images/rook.png');
+
+    newBoard[7][0] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: true,
+        imagePath: 'lib/images/rook.png');
+
+    newBoard[7][7] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: true,
+        imagePath: 'lib/images/rook.png');
+    //place knights
+    newBoard[0][1] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: false,
+        imagePath: 'lib/images/knight.png');
+    newBoard[0][6] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: false,
+        imagePath: 'lib/images/knight.png');
+
+    newBoard[7][1] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: true,
+        imagePath: 'lib/images/knight.png');
+
+    newBoard[7][6] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: true,
+        imagePath: 'lib/images/knight.png');
+    //place bishops
+    newBoard[0][2] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: false,
+        imagePath: 'lib/images/bishop.png');
+    newBoard[0][5] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: false,
+        imagePath: 'lib/images/bishop.png');
+
+    newBoard[7][2] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: true,
+        imagePath: 'lib/images/bishop.png');
+    newBoard[7][5] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: true,
+        imagePath: 'lib/images/bishop.png');
+    //place queens
+    newBoard[0][3] = ChessPiece(
+        type: ChessPieceType.queen,
+        isWhite: false,
+        imagePath: 'lib/images/queen.png');
+
+    newBoard[7][4] = ChessPiece(
+        type: ChessPieceType.queen,
+        isWhite: true,
+        imagePath: 'lib/images/queen.png');
+    //place kings
+    newBoard[0][4] = ChessPiece(
+        type: ChessPieceType.king,
+        isWhite: false,
+        imagePath: 'lib/images/king.png');
+
+    newBoard[7][3] = ChessPiece(
+        type: ChessPieceType.king,
+        isWhite: true,
+        imagePath: 'lib/images/king.png');
     board = newBoard;
   }
 
- /* //create a piece but now we  need _initializeBoard fore the initial one
+  /* //create a piece but now we  need _initializeBoard fore the initial one
   ChessPiece myPawn = ChessPiece(
     type: ChessPieceType.pawn,
     isWhite: true,
@@ -75,8 +146,8 @@ class _GameBoardState extends State<GameBoard> {
             const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
         itemBuilder: (context, index) {
           //get the row and column position of this square
-          int row = index ~/ 8 ;
-          int col = index %8;
+          int row = index ~/ 8;
+          int col = index % 8;
           return Square(
             isWhite: isWhiteT(index),
             piece: board[row][col],
